71426f9ba36ca1e5fa4576094488a7d73b92933b
Dan Lecocq <dan@moz.com>
2016-12-14 10:53:14 -0800

    Version bump to 0.1.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name           = 'gzippy',
-    version          = '0.1.0',
+    version          = '0.1.1',
     description      = 'Gzip file utility.',
     url              = 'http://github.com/seomoz/gzippy',
     author           = 'Dan Lecocq',
