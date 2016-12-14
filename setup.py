078e1315a06d4e6ed5ffbb8da2b9ced77430659a
Dan Lecocq <dan@moz.com>
2016-12-14 10:52:41 -0800

    Fix project url.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name           = 'gzippy',
     version          = '0.1.0',
     description      = 'Gzip file utility.',
-    url              = 'http://github.com/seomoz/batleth',
+    url              = 'http://github.com/seomoz/gzippy',
     author           = 'Dan Lecocq',
     author_email     = 'dan@moz.com',
     py_modules = [
