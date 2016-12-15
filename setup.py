aad492e9d19582e5a5842fd52f4d7c6ca4327db4
Dan Lecocq <dan@moz.com>
2016-12-14 18:07:46 -0800

    Use packages instead of py_modules.

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@
 from distutils.core import setup
 
 setup(name           = 'gzippy',
-    version          = '0.1.1',
+    version          = '0.1.2',
     description      = 'Gzip file utility.',
     url              = 'http://github.com/seomoz/gzippy',
     author           = 'Dan Lecocq',
     author_email     = 'dan@moz.com',
-    py_modules = [
+    packages = [
         'gzippy'
     ],
+    package_dir = {
+        'gzippy': 'gzippy'
+    },
     install_requires = [
     ],
     tests_require    = [
